58b2259211e98c7fee036fcb5b72367b97db06fe
devedm <developer.eddymena@gmail.com>
2021-06-23 03:21:45 -0600

    new files

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Leane Graham",
+        username: "Finley",
+        email: "Leane@Graham.com"
+    },
+    {
+        id: 2,
+        name: "Zahra Hunt",
+        username: "Megan",
+        email: "Zahra@Hunt.com"
+    },
+    {
+        id: 3,
+        name: "Lucie Day",
+        username: "Mikaela",
+        email: "Lucie@Day.com"
+    },
+    {
+        id: 4,
+        name: "Eloise Terry",
+        username: "Mika",
+        email: "Eloise@Terry.com"
+    },
+    {
+        id: 5,
+        name: "Angelina Wells",
+        username: "Chelsey",
+        email: "Angelina@Wells.com"
+    },
+    {
+        id: 6,
+        name: "Claudette Henderson",
+        username: "Kandice",
+        email: "Claudette@Henderson.com"
+    },
+    {
+        id: 7,
+        name: "Stephen Mitchell",
+        username: "Sheldon",
+        email: "Stephen@Mitchell.com"
+    },
+    {
+        id: 8,
+        name: "Alex Hansen",
+        username: "Stevyn",
+        email: "Alex@Hansen.com"
+    },
+    {
+        id: 9,
+        name: "Marie Black",
+        username: "Suzan",
+        email: "Marie@Black.com"
+    },
+    {
+        id: 10,
+        name: "Paula Velasquez",
+        username: "Maria",
+        email: "Paula@Velasquez.com"
+    }
+]
